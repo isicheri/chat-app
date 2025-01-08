74904ca02a5ce02dbd67f3afab2ea9f1e0b8758b
Isicheri <tech2dom@gmail.com>
2025-01-08 01:44:38 +0100

    still coding

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
 import { ConfigModule,ConfigService } from '@nestjs/config';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { TokenService } from './token/token.service';
+
+const pubsub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_HOST || '6379',10),
+    retryStrategy: (times) => {
+      return Math.min(times * 50,2000)
+    }
+  }
+})
+
 
 @Module({
   imports: [AuthModule, UserModule,GraphQLModule.forRootAsync({
@@ -15,11 +28,25 @@ import { ConfigModule,ConfigService } from '@nestjs/config';
     driver: ApolloDriver,
     useFactory: async(
       configService: ConfigService,
-      // tokenService: TokenService
+      tokenService: TokenService
     ) => {
       return {
+        installSubscriptionHandlers: true,
         playground: true,
-        autoSchemaFile: join(process.cwd() , "src/schema.gql")
+        autoSchemaFile: join(process.cwd() , "src/schema.gql"),
+        sortSchema: true,
+        subscriptions: { 
+          'graphql-ws': true,
+          'subscriptions-transport-ws': true
+        },
+        onConnect: (connectionParams) => {
+          const token = tokenService.extractToken(connectionParams)
+         if(!token) {
+          throw new Error("token not provided")
+         }
+         const user =  tokenService.validateToken(token)
+         return {user}
+        }
       }
     }
   }),
@@ -28,6 +55,6 @@ ConfigModule.forRoot({
 })
 ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TokenService],
 })
 export class AppModule {}
