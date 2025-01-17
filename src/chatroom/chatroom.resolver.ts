4207ef6721ed5cc1439ff039e14f70b91dd59673
Isicheri <tech2dom@gmail.com>
2025-01-17 01:15:37 +0100

    working on the resolver

--- src/chatroom/chatroom.resolver.ts
+++ src/chatroom/chatroom.resolver.ts
@@ -1,10 +1,10 @@
-import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver,Query } from '@nestjs/graphql';
 import { ChatroomService } from './chatroom.service';
 import { UserService } from 'src/user/user.service';
 import { UseFilters, UseGuards } from '@nestjs/common';
 import { GraphqlErrorFilter } from 'src/filters/custom-exception.filters';
 import { GraphqlAuthGuard } from 'src/auth/guard/graphql-auth.guard';
-import { Chatroom } from './types/chatroom.types';
+import { Chatroom, Message } from './types/chatroom.types';
 import { Request } from 'express';
 
 @Resolver()
@@ -23,4 +23,30 @@ export class ChatroomResolver {
     ) {
         return this.chatroomService.createChatroom(name,context.req.user?.sub);
     }
+
+
+    @Mutation(() => Chatroom)
+    async addUsersToChatroom(
+        @Args("chatroomId") chatroomId: number,
+        @Args("userIds",{type: () => [Number]}) userIds: number[]
+    ) {
+        return await this.addUsersToChatroom(chatroomId,userIds);
+    }
+
+  
+    @Query(() => [Chatroom])
+    async getChatroomsForUser(
+        @Args("userId") userId: number,
+    ) {
+        return await  this.getChatroomsForUser(userId);
+    }
+
+
+    @Query(() => [Message])
+    async getMessagesForChatroom(
+        @Args("chatroomId") chatroomId: number
+    ) {
+        return this.chatroomService.getMessagesForChatroom(chatroomId)
+    }
+
 }
